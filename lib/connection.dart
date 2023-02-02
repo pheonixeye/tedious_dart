1555dfe0ab52b53ac08e02f4607212796ee1c678
pheonixeye <65448447+pheonixeye@users.noreply.github.com>
2023-02-02 22:48:24 +0200

    connection class constructor

--- lib/connection.dart
+++ lib/connection.dart
@@ -1,13 +1,17 @@
 // ignore_for_file: constant_identifier_names, library_private_types_in_public_api
 
+import 'dart:async';
 import 'dart:io';
 
 import 'package:eventify/eventify.dart';
 import 'package:node_interop/buffer.dart';
 import 'package:tedious_dart/always_encrypted/keystore_provider_azure_key_vault.dart';
+import 'package:tedious_dart/collation.dart';
 import 'package:tedious_dart/message.dart';
+import 'package:tedious_dart/message_io.dart';
 import 'package:tedious_dart/metadata_parser.dart';
 import 'package:tedious_dart/models/errors.dart';
+import 'package:tedious_dart/transient_error_lookup.dart';
 
 typedef BeginTransactionCallback = void Function(
     {Error? err, Buffer? transactionDescriptor});
@@ -74,8 +78,8 @@ class _AuthOptions {
 }
 
 abstract class _Authentication {
-  String get type;
-  _AuthOptions get options;
+  String? get type;
+  _AuthOptions? get options;
 }
 
 class AzureActiveDirectoryMsiAppServiceAuthentication extends _Authentication {
@@ -231,12 +235,12 @@ class InternalConnectionConfig {
   });
 }
 
-class Debug {
+class DebugOptions {
   bool? data;
   bool? packet;
   bool? payload;
   bool? token;
-  Debug({
+  DebugOptions({
     this.data,
     this.packet,
     this.payload,
@@ -264,7 +268,7 @@ abstract class InternalConnectionOptions {
   String? get database;
   num get datefirst;
   String get dateFormat;
-  Debug get debug;
+  DebugOptions get debug;
   bool? get enableAnsiNull;
   bool? get enableAnsiNullDefault;
   bool? get enableAnsiPadding;
@@ -323,23 +327,284 @@ abstract class State {
   _events get events;
 }
 
-enum Authentication {
-  DefaultAuthentication,
-  NtlmAuthentication,
-  AzureActiveDirectoryPasswordAuthentication,
-  AzureActiveDirectoryMsiAppServiceAuthentication,
-  AzureActiveDirectoryMsiVmAuthentication,
-  AzureActiveDirectoryAccessTokenAuthentication,
-  AzureActiveDirectoryServicePrincipalSecret,
-  AzureActiveDirectoryDefaultAuthentication,
-}
+// enum Authentication {
+//   DefaultAuthentication('default'),
+//   NtlmAuthentication('ntlm'),
+//   AzureActiveDirectoryPasswordAuthentication('azure-active-directory-password'),
+//   AzureActiveDirectoryMsiAppServiceAuthentication(
+//       'azure-active-directory-msi-app-service'),
+//   AzureActiveDirectoryMsiVmAuthentication('azure-active-directory-msi-vm'),
+//   AzureActiveDirectoryAccessTokenAuthentication(
+//       'azure-active-directory-access-token'),
+//   AzureActiveDirectoryServicePrincipalSecret(
+//       'azure-active-directory-service-principal-secret'),
+//   AzureActiveDirectoryDefaultAuthentication('azure-active-directory-default');
 
-//TODO: better implementation ==>> get auth type by "String type" attribute
+//   final String type;
+//   const Authentication(this.type);
+// }
+
 class AuthenticationType {
-  Authentication auth;
-  int index;
-  AuthenticationType(this.index)
-      : auth = Authentication.values.elementAt(index);
+  late _Authentication? auth;
+  String type;
+  AuthenticationType(this.type) {
+    switch (type) {
+      case 'ntlm':
+        auth = NtlmAuthentication();
+        break;
+      case 'azure-active-directory-password':
+        auth = AzureActiveDirectoryPasswordAuthentication();
+        break;
+      case 'azure-active-directory-msi-app-service':
+        auth = AzureActiveDirectoryMsiAppServiceAuthentication();
+        break;
+      case 'azure-active-directory-msi-vm':
+        auth = AzureActiveDirectoryMsiVmAuthentication();
+        break;
+      case 'azure-active-directory-access-token':
+        auth = AzureActiveDirectoryAccessTokenAuthentication();
+        break;
+      case 'azure-active-directory-service-principal-secret':
+        auth = AzureActiveDirectoryServicePrincipalSecret();
+        break;
+      case 'azure-active-directory-default':
+        auth = AzureActiveDirectoryDefaultAuthentication();
+        break;
+      case 'default':
+        auth = DefaultAuthentication();
+        break;
+      default:
+        auth = null;
+    }
+  }
 }
 
-class Connection extends EventEmitter {}
+class ConnectionConfiguration {
+  String server;
+
+  ConnectionOptions? options;
+
+  AuthenticationOptions authentication;
+
+  ConnectionConfiguration({
+    required this.server,
+    required this.authentication,
+    this.options,
+  });
+}
+
+class AuthenticationOptions {
+  AuthenticationType? type;
+  dynamic options;
+  AuthenticationOptions({
+    this.type,
+    this.options,
+  });
+}
+
+abstract class ConnectionOptions {
+  bool get abortTransactionOnError;
+  String? get appName;
+  bool get camelCaseColumns;
+  num get cancelTimeout;
+  num get columnEncryptionKeyCacheTTL;
+  bool get columnEncryptionSetting;
+  String? columnNameReplacer({
+    String colName,
+    num index,
+    Metadata metadata,
+  });
+  num get connectionRetryInterval;
+  num get connectTimeout;
+  // connectionIsolationLevel: typeof ISOLATION_LEVEL[keyof typeof ISOLATION_LEVEL];
+  //TODO
+  SecurityContext get cryptoCredentialsDetails;
+  String? get database;
+  num get datefirst;
+  String get dateFormat;
+  DebugOptions get debug;
+  bool? get enableAnsiNull;
+  bool? get enableAnsiNullDefault;
+  bool? get enableAnsiPadding;
+  bool? get enableAnsiWarnings;
+  bool? get enableArithAbort;
+  bool? get enableConcatNullYieldsNull;
+  bool? get enableCursorCloseOnCommit;
+  bool? get enableImplicitTransactions;
+  bool? get enableNumericRoundabort;
+  bool? get enableQuotedIdentifier;
+  bool get encrypt;
+  KeyStoreProviderMap get encryptionKeyStoreProviders;
+  bool get fallbackToDefaultDb;
+  String get instanceName;
+  // isolationLevel: typeof ISOLATION_LEVEL[keyof typeof ISOLATION_LEVEL];
+  String get language;
+  String? get localAddress;
+  num get maxRetriesOnTransientErrors;
+  bool get multiSubnetFailover;
+  num get packetSize;
+  num? get port;
+  bool get readOnlyIntent;
+  num get requestTimeout;
+  bool get rowCollectionOnDone;
+  bool get rowCollectionOnRequestCompletion;
+  String? get serverName;
+  bool get serverSupportsColumnEncryption;
+  String get tdsVersion;
+  num get textsize;
+  String get trustedServerNameAE;
+  bool get trustServerCertificate;
+  bool get useColumnNames;
+  bool get useUTC;
+  String? get workstationId;
+  bool get lowerCaseGuids;
+}
+
+const Map<String, int> CLEANUP_TYPE = {
+  'NORMAL': 0,
+  'REDIRECT': 1,
+  'RETRY': 2,
+};
+
+class RoutingData {
+  String server;
+  num port;
+  RoutingData({
+    required this.server,
+    required this.port,
+  });
+}
+
+class Connection extends EventEmitter {
+  bool fedAuthRequired;
+
+  InternalConnectionConfig? config;
+
+  SecurityContext secureContextOptions;
+
+  bool inTransaction;
+
+  List<Buffer> transactionDescriptors;
+
+  num transactionDepth;
+
+  bool isSqlBatch;
+
+  num curTransientRetryCount;
+
+  TransientErrorLookup transientErrorLookup;
+
+  bool closed;
+
+  Error? loginError;
+  //  undefined | AggregateError | ConnectionError;
+
+  DebugOptions debug;
+
+  dynamic ntlmpacket;
+
+  Buffer? ntlmpacketBuffer;
+
+  State STATE = {};
+
+  RoutingData? routingData;
+
+  MessageIO messageIo;
+
+  State state;
+
+  bool? resetConnectionOnNextRequest;
+
+  dynamic request;
+  //  undefined | Request | BulkLoad;
+
+  dynamic procReturnStatusValue;
+
+  Socket? socket;
+
+  Buffer messageBuffer;
+
+  Timer? connectTimer;
+
+  Timer? cancelTimer;
+
+  Timer? requestTimer;
+
+  Timer? retryTimer;
+
+  //todo
+  void _cancelAfterRequestSent;
+
+  Collation? databaseCollation;
+
+  Connection(this.config) : super() {
+    //
+    if (config.runtimeType != Object || config == null) {
+      throw MTypeError(
+          'The "config" argument is required and must be of type Object.');
+    }
+
+    if (config!.server.runtimeType != String) {
+      throw MTypeError(
+          'The "config.server" property is required and must be of type string.');
+    }
+
+    fedAuthRequired = false;
+
+    AuthenticationType authentication;
+    if (config!.authentication != null) {
+      if (config!.authentication.runtimeType != Object ||
+          config!.authentication == null) {
+        throw MTypeError(
+            'The "config.authentication" property must be of type Object.');
+      }
+
+      var type = config!.authentication!.type;
+      var options = config!.authentication!.options;
+
+      if (type.runtimeType != String) {
+        throw MTypeError(
+            'The "config.authentication.type" property must be of type string.');
+      }
+      if (type != 'default' &&
+          type != 'ntlm' &&
+          type != 'azure-active-directory-password' &&
+          type != 'azure-active-directory-access-token' &&
+          type != 'azure-active-directory-msi-vm' &&
+          type != 'azure-active-directory-msi-app-service' &&
+          type != 'azure-active-directory-service-principal-secret' &&
+          type != 'azure-active-directory-default') {
+        throw MTypeError(
+            'The "type" property must one of "default", "ntlm", "azure-active-directory-password", "azure-active-directory-access-token", "azure-active-directory-default", "azure-active-directory-msi-vm" or "azure-active-directory-msi-app-service" or "azure-active-directory-service-principal-secret".');
+      }
+      if (options.runtimeType != Object || options == null) {
+        throw MTypeError(
+            'The "config.authentication.options" property must be of type object.');
+      }
+      if (type == 'ntlm') {
+        if (options.domain.runtimeType != String) {
+          throw MTypeError(
+              'The "config.authentication.options.domain" property must be of type string.');
+        }
+
+        if (options.userName != null &&
+            options.userName.runtimeType != String) {
+          throw MTypeError(
+              'The "config.authentication.options.userName" property must be of type string.');
+        }
+
+        if (options.password != null &&
+            options.password.runtimeType != String) {
+          throw MTypeError(
+              'The "config.authentication.options.password" property must be of type string.');
+        }
+        authentication = AuthenticationType(type!)
+          ..auth = NtlmAuthentication(
+            userName: options.userName,
+            password: options.password,
+            domain: options.domain,
+          );
+      } else if (type == 'azure-active-directory-password') {}
+    }
+  }
+}
